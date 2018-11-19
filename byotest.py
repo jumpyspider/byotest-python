4fc295b01e5e47d2a82219651dc95eb28a3fa218
David Roberts <cezroberts@hotmail.com>
2018-11-19 12:54:47 +0000

    Vending Machine complete with all challenges

--- byotest.py
+++ byotest.py
@@ -1,12 +1,12 @@
-
 def test_are_equal(actual, expected):
-     """
+    """
     Test that two values are equal. Raises AssertionError if both values are
     not equal.
     `actual` is the actual value produced
     `expected` is the value that was supposed to be produced
     """
-    assert expected == actual, "Expected {0}, got {1}".format(expected, actual)
+    assert expected == actual, "Expected {0}, got {1}".format(
+        expected, actual)
 
 
 def test_not_equal(a, b):
@@ -26,7 +26,8 @@ def test_is_in(collection, item):
     `collection` is the collection to be tested
     `item` is the item that is being searched for
     """
-    assert item in collection, "{0} does not contain {1}".format(collection, item)
+    assert item in collection, "{0} does not contain {1}".format(
+        collection, item)
 
 
 def test_not_in(collection, item):
@@ -36,9 +37,10 @@ def test_not_in(collection, item):
     `collection` is the collection in question
     `item` is the thing that we want to check for
     """
-    assert item not in collection, "{0} is not in {1}".format(item, collection)
-    
-    
+    assert item not in collection, "{0} is not in {1}".format(
+        item, collection)
+
+
 def test_between(upper_limit, lower_limit, actual):
     """
     Check to ensure that a number is between two other numbers. Raises
@@ -47,17 +49,32 @@ def test_between(upper_limit, lower_limit, actual):
     assert lower_limit <= actual <= upper_limit, "{0} is not between {1} and {2}".format(actual, lower_limit, upper_limit)
 
 
-# Test to fail the `test_are_equal` function
-# test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+def test_exception_was_raised(func, args, message):
+    """
+    Test that an error gets thrown inside of a given function. Raises
+    AssertionError if the error message was different from the expected
+    message
+    `func` is a reference to the function that is to be called
+    `args` is a tuple containing the arguments that are to be provided to
+        `func`
+    `message` is the string that is expected to be output by the error once
+        it's thrown
+    """
+    try:
+        # Call the function and unpack the `args` tuple by using `*`. This
+        # will unpack each of the items from the `args` tuple to pass 
+        # them into the function as arguments
+        func(*args)
 
-# Test to fail the `test_not_equal` function
-# test_not_equal(0, 0)
-
-# Test to fail the `test_is_in` function
-# test_is_in([1], 2)
-
-# Test to fail the `test_not_in` function
-# test_not_in([1], 1)
-
-# Test to fail the `test_between` function
-# test_between(10, 1, 200)
+        # Execution will cease at this point if the error was successfully
+        # thrown, and will move onto the `except` block. If the
+        # function was successfully executed without throwing an error, we'll
+        # raise an AssertionError here to inform the developer that the
+        # function didn't throw an error as expected
+        assert False, "Exception was not raised"
+    except Exception as e:
+        # The message that was thrown will be stored in the exception
+        # instance as the first item in the list of `args`. This will allow us
+        # to check to see if the message that was thrown is the same as the
+        # message that the developer was expecting 
+        assert e.args[0] == message, "The message that was provided did not match the message thrown"
